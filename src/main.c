62d18f4e160e1fc6ea1263837fe89164690a33b2
kmdailey76 <kmdailey76@gmail.com>
2014-10-18 22:52:15 -0500

    v3.1 with weather

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <pebble.h>
+  #define KEY_TEMPERATURE 0
+  #define KEY_CONDITIONS 1
 
+  //Store incoming information
+  
   static Window *s_main_window;
 
   static TextLayer *s_weather_layer;
@@ -13,6 +17,52 @@
   static BitmapLayer *s_background_layer;
 
   static GBitmap *s_background_bitmap;
+
+  static void inbox_received_callback(DictionaryIterator *iterator, void *context){
+    
+    //Store incoming information
+    static char temperature_buffer[8];
+    static char conditions_buffer[32];
+    static char weather_layer_buffer[32];
+    
+    //Read first item
+    Tuple *t = dict_read_first(iterator);
+    
+    //For all items
+    while(t != NULL){
+      //Which key was received?
+      switch(t->key) {
+        case KEY_TEMPERATURE:
+        snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
+        break;
+        case KEY_CONDITIONS:
+        snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+        break;
+        default:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+        break;
+      }
+      
+      //Assemble full string and display
+      snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+      text_layer_set_text(s_weather_layer, weather_layer_buffer);
+      
+      //Look for next item
+      t = dict_read_next(iterator);
+    }
+  }
+
+  static void inbox_dropped_callback(AppMessageResult reason, void *context){
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+  }
+
+  static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context){
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+  }
+
+  static void outbox_sent_callback(DictionaryIterator *iterator, void *context){
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+  }
 
   static void update_time() {
     // Get a tm structure
@@ -56,7 +106,9 @@
     text_layer_set_background_color(s_weather_layer, GColorClear);
     text_layer_set_text_color(s_weather_layer, GColorWhite);
     text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
-    text_layer_set_text(s_weather_layer, "Sara Smells");
+    //text_layer_set_text(s_weather_layer, "Sara Smells");
+    //snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    //text_layer_set_text(s_weather_layer, weather_layer_buffer);
         
     //apply to TextLayer
     text_layer_set_font(s_time_layer, s_time_font);
@@ -110,9 +162,20 @@
     
   // Register with TickTimerService
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-}
+    
+  //Register Callbacks
+    app_message_register_inbox_received(inbox_received_callback);
+    
+  //Open AppMessage
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
+      
+  } 
 
-static void deinit() {
+ static void deinit() {
   // Destroy Window
   window_destroy(s_main_window);
 }
